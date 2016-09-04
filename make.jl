87247287c4858de8faba5d512203b557bd4ac13b
monty <velimir.vesselinov@gmail.com>
2016-09-04 08:10:23 -0700

    Deployed 33972a5 with MkDocs version: 0.15.3

---
+++ make.jl
@@ -0,0 +1,13 @@
+import Documenter
+import Mads
+
+Documenter.makedocs(
+    modules = [Mads]
+)
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+#=Documenter.deploydocs(
+    repo = "<repository url>"
+)=#
